f460192b7f4eb4dbf83066e7599c9275002b5359
Christopher N. Hesse <raymanfx@gmail.com>
2018-01-28 13:17:25 +0100

    a5y17lte: Remove TWRP cruft from branch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,25 +204,9 @@ TARGET_INIT_VENDOR_LIB := libinit_sec
 #TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
 
 # Recovery
-#RECOVERY_VARIANT := twrp
 BOARD_HAS_DOWNLOAD_MODE := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.samsungexynos7880
 TARGET_OTA_ASSERT_DEVICE := a5y17lte,a5y17ltexx
-
-# TWRP
-ifeq ($(RECOVERY_VARIANT),twrp)
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/twrp.fstab
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_THEME := portrait_hdpi
-TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
-TW_MAX_BRIGHTNESS := 255
-TW_NO_REBOOT_BOOTLOADER := true
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_NTFS_3G := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_NO_EXFAT_FUSE := true
-TW_EXCLUDE_SUPERSU := true
-endif
 
 # Seccomp filters
 BOARD_SECCOMP_POLICY += device/samsung/a5y17lte/seccomp
